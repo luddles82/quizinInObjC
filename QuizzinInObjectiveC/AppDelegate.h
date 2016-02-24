7d3494ff3ce29125ee44aaf6a9f934a473016634
Nick Ludlow <nickludlow82@gmail.com>
2016-02-24 17:21:02 +0000

    Initial Commit

---
+++ QuizzinInObjectiveC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QuizzinInObjectiveC
+//
+//  Created by Nick Ludlow on 24/02/2016.
+//  Copyright © 2016 Nick Ludlow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QuizzinInObjectiveC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuizzinInObjectiveC
+//
+//  Created by Nick Ludlow on 24/02/2016.
+//  Copyright © 2016 Nick Ludlow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
